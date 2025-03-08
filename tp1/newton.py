5e649725b17a2f024c9f0e24dc69e11b7c600e50
PERRYNEMO <67051292+PERRYNEMO@users.noreply.github.com>
2025-03-08 10:39:46 +0100

    add newton.py algorythm

--- tp1/newton.py
+++ tp1/newton.py
@@ -1,1 +1,11 @@
 
+def newton(f, df, x0, eps=1e-16, delta=1e-16, max_iter=100):
+    x=x0
+    for i in range(max_iter):
+        fx=f(x)
+        dfx=df(x)
+        x0 = x
+        x = x - fx*dfx/dfx
+        if abs(x0-x)<eps*(abs(x)+abs(x0))+delta:
+            return x
+    return x
