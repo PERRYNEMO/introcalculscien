d45d542116ea98b81595f35872d8344dd7bdbc49
PERRYNEMO <67051292+PERRYNEMO@users.noreply.github.com>
2025-03-08 13:07:30 +0100

    add doc and typing to newton

--- tp1/newton.py
+++ tp1/newton.py
@@ -1,17 +1,52 @@
 """"""
 import math
-from typing import *
+from typing import Callable
 
 
-def relative_error(eps=1e-16, delta=1e-16):
+def relative_error(eps: float = 1e-16, delta: float = 1e-16) \
+        -> Callable[[float, float], bool]:
+    """create relative error function
+
+    :param eps: relative precision (if a and b are big)
+    :param delta: absolute precision (if a and b are close to 0)
+    :return: lamda function that tells if a and b are close enough
+    """
     return lambda a,b:abs(b-a)<eps*(abs(a)+abs(b))+delta
 
-def absolute_error(eps=1e-16):
+def absolute_error(eps: float= 1e-16) \
+        -> Callable[[float, float], bool]:
+    """create absolute error function
+
+    :param eps: precision
+    :return: lamda function that tells if a and b are close enough
+    """
     return lambda a,b:abs(b-a)<eps
 
-def newton(f, df, x0, stop_condition=relative_error(), max_iter=100):
+
+
+def newton(
+        f: Callable[[float], float],
+        df: Callable[[float], float], x0: float,
+        stop_condition : Callable[[float, float], bool] = relative_error(),
+        max_iter: int=100
+) -> float:
+    """find the root of a function using Newton's method
+
+    :param f: The function for which the root is sought
+    :param df: The derivative of the function f
+    :param x0: Initial guess for the root.
+    :param stop_condition: a function that tells if a and b are close enough
+    :param max_iter: Maximum number of iterations (default is 100)
+    :return: The computed root
+
+    :raises ValueError: if x0 is finite, if f(x) or f/'(x) is not defined ,
+    If the derivative is too close to zero, indicating a
+    risk of divergence.
+    :raises RuntimeError: If the maximum number of iterations is reached
+    without convergence.
+    """
     if not math.isfinite(x0):
-        raise AssertionError("x0 is not finite")
+        raise ValueError("x0 is not finite")
     x=x0
     n = 0
     while not stop_condition(x, x0) and n <= max_iter:
@@ -19,16 +54,16 @@ def newton(f, df, x0, stop_condition=relative_error(), max_iter=100):
         fx=f(x)
         dfx=df(x)
         if not math.isfinite(fx):
-            raise AssertionError('f is not defined at '+x)
+            raise ValueError('f is not defined at '+x)
         if not math.isfinite(dfx):
-            raise AssertionError('f derivative is not defined at '+x)
+            raise ValueError('f derivative is not defined at '+x)
         if abs(dfx)<1e-12:
-            raise AssertionError("f derivative to small at "+x+" newton did not converge")
+            raise ValueError("f derivative to small at "+x+" newton did not converge")
         x0 = x
         x = x - fx/dfx
     if n <= max_iter:
         return x
-    raise Exception("Newton did not converge")
+    raise RuntimeError("Newton did not converge")
 
 
 def bisection(f, a, b, stop_condition=relative_error(), max_iter=100):
