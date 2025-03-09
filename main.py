ed8bbbee1d329c6eb35764e33d14983d9cc412b1
PERRYNEMO <67051292+PERRYNEMO@users.noreply.github.com>
2025-03-09 23:19:02 +0100

    calculate some values

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def main():
-    return 'hi', 'he'
+from tp1.root_finding import continu
+
+if __name__ == "__main__":
+    s1 = continu(1)
+    s2 = continu(2)
+    s3 = continu(1e-6)
+    print(s1, s2, s3)
