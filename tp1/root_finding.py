6f58f46024eee44f492edc2ecb86d5ce30add322
PERRYNEMO <67051292+PERRYNEMO@users.noreply.github.com>
2025-03-09 23:14:23 +0100

    add docs to root finding

--- tp1/root_finding.py
+++ tp1/root_finding.py
@@ -1,5 +1,5 @@
 """
-
+Calculate the value of a and b for which the function is continuous
 """
 import math
 
@@ -64,16 +64,18 @@ def h(b: float) -> float:
 
 def find_last_root(b: float, x0: float, f: Function, df: Function,
                    max_iter: int = 100) -> float:
-    """
+    """calculate the root of the function, the only thing we know is
+    the derivative is croissant, the derivative of x0 is positive, and there is
+    one root between x0 and + infinity
 
-    :param b:
-    :param x0:
-    :param f:
-    :param df:
-    :param max_iter:
-    :return:
+    :param b: parameter
+    :param x0: the starting value
+    :param f: the function that we want to find the root
+    :param df: the derivative of f
+    :param max_iter: maximum iterations
+    :return: the root of the function
 
-    :raise ValueError: if the function is not defined
+    :raise ValueError: if the function gb do an overflow at the root
     """
     n: int = 0
     k: float = math.log(1 / b) * 0.1
@@ -92,28 +94,39 @@ def find_last_root(b: float, x0: float, f: Function, df: Function,
         n = n + 1
         k /= 1.05
     if n > max_iter:
-        raise ValueError("x0 too high")
+        raise ValueError("we can not find the interval, probably "
+                         "the root is to high, the limit for the exp is 710")
     return find_root_best(f, df, x0, tk)
 
 
 def find_b_star():
+    """ find when there is only one root of gb on positif
+
+    :return: the value of b when the root of gb and his derivative are the same
+    """
     return bisection(lambda b: get_gb(b)(h(b)), 1, 1.5)
 
 
 b_star = find_b_star()
 
 
-def continu(b):
-    if b == 0:
-        return -4, 0
+def continu(b: float) -> list[float]:
+    """calculate the value of a for a b for which the function is continuous
+    so the root of gb
 
-    a1 = find_root_best(get_gb(b), get_derivative_gb(b), -6, 0)
+    :param b: parameter
+    :return: the list of the roots
+    """
+    if b == 0:
+        return [-4, 0]
+
+    a1: float = find_root_best(get_gb(b), get_derivative_gb(b), -6, 0)
 
     if abs(b) > b_star:
-        return a1
+        return [a1]
     if abs(b) == b_star:
-        return a1, h(b)
-    hb = h(b)
-    a2 = find_root_best(get_gb(b), get_derivative_gb(b), -0.5, hb)
-    a3 = find_last_root(b, hb, get_gb(b), get_derivative_gb(b))
-    return a1, a2, a3
+        return [a1, h(b)]
+    hb: float = h(b)
+    a2: float = find_root_best(get_gb(b), get_derivative_gb(b), -0.5, hb)
+    a3: float = find_last_root(b, hb, get_gb(b), get_derivative_gb(b))
+    return [a1, a2, a3]
