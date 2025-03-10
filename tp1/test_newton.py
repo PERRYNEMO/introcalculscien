0e338c93e5ca6da4f5bafe589abc7d8b1b030331
PERRYNEMO <67051292+PERRYNEMO@users.noreply.github.com>
2025-03-10 21:54:28 +0100

    calculate some values

--- tp1/test_newton.py
+++ tp1/test_newton.py
@@ -1,9 +1,12 @@
-from tp1.newton import *
 import math
+
+from tp1.newton import *
+
+
 def test_newton():
-    square = lambda x: x ** 2-2
-    dsquare = lambda x: 2*x
-    assert newton(square, dsquare, 1) - math.sqrt(2)<1e-16
-    assert  -math.sqrt(2)-newton(square, dsquare, -1)<1e-16
-    assert bisection(square, 0, 4) - math.sqrt(2)<1e-16
+    square = lambda x: x ** 2 - 2
+    dsquare = lambda x: 2 * x
+    assert newton(square, dsquare, 1) - math.sqrt(2) < 1e-16
+    assert -math.sqrt(2) - newton(square, dsquare, -1) < 1e-16
+    assert bisection(square, 0, 4) - math.sqrt(2) < 1e-16
     assert bisection(square, 4, 0) - math.sqrt(2) < 1e-16
